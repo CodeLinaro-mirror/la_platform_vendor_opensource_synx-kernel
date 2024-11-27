128bd950ca64c7c56d75fde62104d5c7bff40e75
vchollan <quic_vchollan@quicinc.com>
2024-11-27 18:18:07 +0530

    msm: synx: print hw fence for synx_import during interop usecase
    Add check while updating new_h_synx parameter in get_handle_status
    
    Change-Id: I08b0c063d8ee5307d535f77367faba82d592382a
    Signed-off-by: vchollan <quic_vchollan@quicinc.com>
    (cherry picked from commit f008891b2f910f802ea74a5f7aed71c7a19098de)

--- msm/synx/synx.c
+++ msm/synx/synx.c
@@ -1856,7 +1856,8 @@ static int synx_internal_get_handle_status(struct synx_import_indv_params *param
 		goto bail;
 	}
 
-	*params->new_h_synx = h_synx;
+	if (is_waiter)
+		*params->new_h_synx = h_synx;
 
 bail:
 	*signal_status = status;
