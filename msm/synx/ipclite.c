88c30445d847af9d5305f8094787d34da09d481f
Snehalkumar Parmar <snehalku@qti.qualcomm.com>
2025-04-30 23:28:28 +0530

    Handled calling of set_ipcmem_access_control
    Change-Id: Ied7c8de5b066ee64ae66780c224fac284e3c1532
    Signed-off-by: Snehalkumar Parmar <snehalku@qti.qualcomm.com>

--- msm/synx/ipclite.c
+++ msm/synx/ipclite.c
@@ -1011,6 +1011,10 @@ static void ipcmem_init(struct ipclite_mem *ipcmem)
 static int set_ipcmem_access_control(struct ipclite_info *ipclite)
 {
 	int ret = 0;
+
+	if (ipclite == NULL)
+		return ret;
+
 #if (KERNEL_VERSION(5, 15, 0) < LINUX_VERSION_CODE)
 		uint64_t dspVMids = BIT(VMID_HLOS) | BIT(VMID_CDSP);
 		struct qcom_scm_vmperm destVM[2] = {
@@ -1605,6 +1609,8 @@ static int ipclite_driver_restore(struct device *dev)
 			dev_err(dev, "failed to setup ipclite mem\n");
 			return -EINVAL;
 		}
+		#else
+		ret = set_ipcmem_access_control(NULL);
 		#endif
 	}
 
