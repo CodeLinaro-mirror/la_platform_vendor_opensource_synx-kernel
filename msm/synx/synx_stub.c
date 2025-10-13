d8e70007e4281f1c592b8491f968a8205c0c425d
Grace An <gracan@qti.qualcomm.com>
2025-10-13 13:18:47 -0700

    msm: synx: ignore error in synx stub init for hw-fence not initialized
    In synx_stub_init, ignore error from synx_hwfence_init_ops in the same
    way that this error is ignored for synx_driver_init. This ensures that
    this module can be insmod and initialized even if hw-fence config flag
    is false.
    
    Change-Id: Icc725579d694af8c0a4c3f6e77f1c662e87b2199
    Signed-off-by: Grace An <gracan@qti.qualcomm.com>

--- msm/synx/synx_stub.c
+++ msm/synx/synx_stub.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2025, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 #include <linux/module.h>
 #include "synx_api.h"
@@ -23,7 +23,14 @@ int synx_internal_recover(enum synx_client_id id)
 
 static int __init synx_init(void)
 {
-	return synx_hwfence_init_ops(&synx_hwfence_ops);
+	int rc;
+
+	/* ignore error because Synx API is still functional even if an error is returned */
+	rc = synx_hwfence_init_ops(&synx_hwfence_ops);
+	if (rc)
+		dprintk(SYNX_DBG, "hwfence is not supported through synx api, err=%d\n", rc);
+
+	return 0;
 }
 
 module_init(synx_init);
